a5b618b7d9577572613351a9449b1345ddf3e11b
Jinson02 <jcollaguazoa@unemi.edu.ec>
2024-07-04 23:39:11 -0500

    comit final del proyecto

--- sistema_eventos/urls.py
+++ sistema_eventos/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import eventos
+
 from eventos.views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(eventos.urls)),
+    path('', include('eventos.urls')),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
 
